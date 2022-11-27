4a020d089c0abb8bff688d8d967b17a0d3e6b864
Mercia <68623425+mercietc@users.noreply.github.com>
2022-11-27 12:58:40 -0300

    analise do tempo de amarracao linguagem dinamica

---
+++ 2.2.2/linguagemDinamica.py
@@ -0,0 +1,17 @@
+import math
+
+def f(x):
+    return math.sin(x)/10
+#nome = sin, propriedade = implementacao, tempo = ligacao
+
+print(10/10)
+#nome = 1, propriedade = valor, tempo = compilacao
+
+x = [30,40,50]
+#nome = x, propriedade = endereco, tempo = execucao
+#nome = x, propriedade = tipo, tempo = execucao
+
+for i in range(0,len(x)):
+    print(f(x))
+    #nome = print, propriedade = definicao, tempo = implementacao
+    
